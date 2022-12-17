cdd77475c67f3f6b156ba11689d51fac2ee73841
sangitasarkar74 <sangitasarkar@gmail.com>
2022-12-17 10:32:21 +0530

    Stage1

---
+++ script.js
@@ -0,0 +1,9 @@
+const explore = document.querySelector('#explore');
+
+const handleClick = () => {
+    window.location.href = 'https://www.instagram.com ';
+}
+
+explore.addEventListener("click", { handleClick })
+
+
